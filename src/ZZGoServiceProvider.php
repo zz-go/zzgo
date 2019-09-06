7803ac45a5e0e7dcccb11c8e70e1c3bffb89884b
Manfred John <manfred.john@agentur-loop.com>
2019-09-06 02:10:07 +0200

    Add API for datadefinitions

--- src/ZZGoServiceProvider.php
+++ src/ZZGoServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace ZZGo;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use ZZGo\Console\ZZGo;
+use ZZGo\Models\DataDefinition;
 
 class ZZGoServiceProvider extends ServiceProvider
 {
@@ -20,7 +22,19 @@ class ZZGoServiceProvider extends ServiceProvider
             ]);
         }
 
+
+
+        Route::model('data_definition', DataDefinition::class);
+
+
+
+        //Migrations
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        //Routes
+        Route::group($this->routeConfiguration(), function () {
+            $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
+        });
     }
 
     /**
@@ -31,5 +45,21 @@ class ZZGoServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+    /**
+     * Get the Nova route group configuration array.
+     *
+     * @return array
+     */
+    protected function routeConfiguration()
+    {
+        return [
+            'namespace' => 'ZZGo\Http\Controllers',
+            'domain' => config('nova.domain', null),
+            'as' => 'zzgo.api.',
+            'prefix' => 'zzgo',
+            'middleware' => 'api',
+        ];
     }
 }
